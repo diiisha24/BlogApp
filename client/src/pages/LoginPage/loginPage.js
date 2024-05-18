1d7c1e39b23a9fa55442c75bf3f0cc7e8e064e88
Disha Garg <92861286+diiisha24@users.noreply.github.com>
2024-05-18 12:29:36 +0530

    Update loginPage.js

--- client/src/pages/LoginPage/loginPage.js
+++ client/src/pages/LoginPage/loginPage.js
@@ -11,7 +11,7 @@ const LoginPage = () => {
 
   async function login(e){
     e.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
+    const response = await fetch('https://dee-blog-app-api.vercel.app/login', {
       method: 'POST',
       body: JSON.stringify({
         username,
