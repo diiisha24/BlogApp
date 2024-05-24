7516fac050dd0b1daa80b151df2954606b6b0184
Disha Garg <gargdisha1420@gmail.com>
2024-05-24 11:56:08 +0530

    Api updated.

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 const express = require('express'); 
 const cors = require('cors');
-// const axios = require('axios');
+const axios = require('axios');
 const mongoose = require('mongoose');
-// const User = require('./models/user');
-// const Post = require('./models/post');
-// const bycrypt = require('bcrypt');
+const User = require('./models/user');
+const Post = require('./models/post');
+const bycrypt = require('bcrypt');
 const app = express();
-// const jwt = require('jsonwebtoken');
-// const saltRounds = 10;
-// const salt = bycrypt.genSaltSync(saltRounds);
-// const secret = 'deeewrldfwbblogspotcom';
-// const cookieParser = require('cookie-parser');
-// const multer = require('multer');
-// const uploadMiddleware = multer({ dest: 'uploads/' })
-// const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
+const salt = bycrypt.genSaltSync(saltRounds);
+const secret = 'deeewrldfwbblogspotcom';
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' })
+const fs = require('fs');
 
 app.use(cors({
     credentials: true, 
@@ -23,8 +23,8 @@ app.use(cors({
     methods: ["POST", "GET"]
 }));
 
-// app.use(express.json());
-// app.use(cookieParser());
+app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 4000;
 
@@ -58,94 +58,94 @@ app.get('/api', async(req, res) => {
     res.json({message: "Home Page"});
 });
 
-// app.post('/signup', async (req, res) => {
-//     const { username, email, password, confirmPassword } = req.body;
-//     if(password === confirmPassword){
-//         const hashedPassword = await bycrypt.hash(password, salt);
-//         try{
-//                 const user = await User.create({ username, email, password: hashedPassword});
-//                 res.json({ user });
-//         }
-//         catch(error){
-//             res.status(400).json(error);
-//         }
-//     }
-// });
+app.post('/signup', async (req, res) => {
+    const { username, email, password, confirmPassword } = req.body;
+    if(password === confirmPassword){
+        const hashedPassword = await bycrypt.hash(password, salt);
+        try{
+                const user = await User.create({ username, email, password: hashedPassword});
+                res.json({ user });
+        }
+        catch(error){
+            res.status(400).json(error);
+        }
+    }
+});
 
-// app.post('/login', async (req, res) => {
-//     const { identifier, password } = req.body;
-//     try{
-//         const isEmail = identifier.includes('@');
-//         const query = isEmail ? { email: identifier } : { username: identifier };
+app.post('/login', async (req, res) => {
+    const { identifier, password } = req.body;
+    try{
+        const isEmail = identifier.includes('@');
+        const query = isEmail ? { email: identifier } : { username: identifier };
     
-//         const user = await User.findOne(query);
-//         console.log(user);
-//         if(user){
-//             const isMatch = await bycrypt.compare(password, user.password);
-//             if(isMatch){
-//                 // res.json({ user });
-//                 jwt.sign({username: user.username, id: user.id}, secret, {}, (err, token) => {
-//                     if(err) throw err;
-//                     res.cookie('token', token).json({
-//                         id: user._id,
-//                         username: user.username,
-//                     });
-//                 }
-//                 );
-//             }
-//             else{
-//                 res.status(400).json({ error: "Invalid Credentials1!!!" });
-//             }
-//         }
-//         else{
-//             res.status(400).json({ error: "Invalid Credentials2!!!" });
-//         }
-//     }
-//     catch(error){
-//         res.status(400).json(error);
-//     }
-// });
+        const user = await User.findOne(query);
+        console.log(user);
+        if(user){
+            const isMatch = await bycrypt.compare(password, user.password);
+            if(isMatch){
+                // res.json({ user });
+                jwt.sign({username: user.username, id: user.id}, secret, {}, (err, token) => {
+                    if(err) throw err;
+                    res.cookie('token', token).json({
+                        id: user._id,
+                        username: user.username,
+                    });
+                }
+                );
+            }
+            else{
+                res.status(400).json({ error: "Invalid Credentials1!!!" });
+            }
+        }
+        else{
+            res.status(400).json({ error: "Invalid Credentials2!!!" });
+        }
+    }
+    catch(error){
+        res.status(400).json(error);
+    }
+});
 
-// app.get('/profile', async (req, res) => {
-//     const token = req.cookies.token;
-//     if(token){
-//         jwt.verify(token, secret,{}, async (err, info) => {
-//             // if(err) throw err;
-//             if(err){
-//                 console.log("Error",err);
-//             }
-//             res.json(info);
-//         });
-//     }
-// })
+app.get('/profile', async (req, res) => {
+    const token = req.cookies.token;
+    if(token){
+        jwt.verify(token, secret,{}, async (err, info) => {
+            // if(err) throw err;
+            if(err){
+                console.log("Error",err);
+            }
+            res.json(info);
+        });
+    }
+})
 
-// app.post('/post', uploadMiddleware.single('file'),(req,res)=>{
-//     // console.log(req.body);
-//     const {originalname,path} = req.file;
-//     const {title,summary,description} = req.body;
-//     const parts = originalname.split(".");
-//     const ext = parts[parts.length - 1];
-//     const newPath = path + "." + ext;
-//     fs.renameSync(path, newPath);
-//     // console.log(newPath);
-//     // try{
+app.post('/post', uploadMiddleware.single('file'),(req,res)=>{
+    // console.log(req.body);
+    const {originalname,path} = req.file;
+    const {title,summary,description} = req.body;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    // console.log(newPath);
+    // try{
 
-//     // }
-//     res.json({title,summary,description});
+    // }
+    res.json({title,summary,description});
     
-//     res.cookie('token', '').json("ok");
-//     alert("Post Created Successfully!!");
-// })
+    res.cookie('token', '').json("ok");
+    alert("Post Created Successfully!!");
+})
 
-// app.get('/posts', async (req, res) => {
-//     const posts = await Post.find();
-//     res.json(posts);
-// })
+app.get('/posts', async (req, res) => {
+    const posts = await Post.find();
+    res.json(posts);
+})
 
 
-// app.post('/logout', (req, res) => {
-//     res.cookie('token', '').json("ok");
-// })
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json("ok");
+})
 
 app.listen(4000, () => console.log('Server running on port 4000'));
 
